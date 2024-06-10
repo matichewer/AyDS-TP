1b2793b5a25d77ef401ef65fcccd8869c393098c
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-06-10 00:51:46 -0300

    solution

--- src/main/java/ayds/apolo/songinfo/home/model/repository/SongRepository.kt
+++ src/main/java/ayds/apolo/songinfo/home/model/repository/SongRepository.kt
@@ -3,90 +3,48 @@ package ayds.apolo.songinfo.home.model.repository
 import ayds.apolo.songinfo.home.model.entities.EmptySong
 import ayds.apolo.songinfo.home.model.entities.Song
 import ayds.apolo.songinfo.home.model.entities.SpotifySong
-import ayds.apolo.songinfo.home.model.repository.external.spotify.SpotifyModule
+import ayds.apolo.songinfo.home.model.repository.external.spotify.SongBroker
 import ayds.apolo.songinfo.home.model.repository.local.spotify.SpotifyLocalStorage
-import ayds.apolo.songinfo.home.model.repository.local.spotify.sqldb.ResultSetToSpotifySongMapperImpl
-import ayds.apolo.songinfo.home.model.repository.local.spotify.sqldb.SpotifySqlDBImpl
-import ayds.apolo.songinfo.home.model.repository.local.spotify.sqldb.SpotifySqlQueriesImpl
 import ayds.apolo.songinfo.home.model.repository.local.spotify.cache.SongCache
-import com.google.gson.Gson
-import com.google.gson.JsonObject
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import java.io.IOException
 
 
 interface SongRepository {
     fun getSongByTerm(term: String): Song
 }
 
-
-internal class SongRepositoryImp (
+internal class SongRepositoryImpl(
     private val songCache: SongCache,
-    private val songLocalStorage: SpotifyLocalStorage
-){
+    private val songLocalStorage: SpotifyLocalStorage,
+    private val songBroker: SongBroker,
+) : SongRepository {
 
+    override fun getSongByTerm(term: String): Song {
+        var song = songCache.getSongByTerm(term)
 
-
-    internal val spotifyDB = SpotifySqlDBImpl(
-        SpotifySqlQueriesImpl(), ResultSetToSpotifySongMapperImpl()
-    )
-    val spotifyTrackService = SpotifyModule.spotifyTrackService
-
-
-    ///// Wiki
-    var retrofit: Retrofit? = Retrofit.Builder()
-        .baseUrl("https://en.wikipedia.org/w/")
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .build()
-
-    var wikipediaAPI = retrofit!!.create(WikipediaAPI::class.java)
-    //// end wiki
-
-    fun getSongByTerm(term: String): SearchResult {
-
-            var spotifySong: SpotifySong? = songCache.getSongByTerm(term)
-        if (spotifySong != null) {
-            spotifySong.isCacheStored = true
-            return spotifySong
+        if (song != null) {
+            song.setSongIsCacheStored()
         } else {
-            spotifySong = spotifyDB.getSongByTerm(term)
-            if (spotifySong != null) {
-                spotifySong.isLocallyStored = true
-                // update the cache
-                cache[term] = spotifySong
-                return spotifySong
+            song = songLocalStorage.getSongByTerm(term)
+            if (song != null) {
+                song.setSongIsLocallyStored()
+                songCache.insertSong(term, song)
             } else {
-                spotifySong = spotifyTrackService.getSong(term)
-                if (spotifySong != null) {
-                    spotifyDB.insertSong(term, spotifySong)
-                    return spotifySong
+                song = songBroker.getSong(term)
+                if (song != null) {
+                    songCache.insertSong(term, song)
+                    songLocalStorage.insertSong(term, song)
                 }
             }
-
-        /////// Last chance, get anything from the wiki
-        val callResponse: Response<String>
-        try {
-            callResponse = wikipediaAPI.getInfo(term).execute()
-            System.out.println("JSON " + callResponse.body())
-            val gson = Gson()
-            val jobj: JsonObject = gson.fromJson(callResponse.body(), JsonObject::class.java)
-            val query = jobj["query"].asJsonObject
-            val snippetObj = query["search"].asJsonArray.firstOrNull()
-            if (snippetObj != null) {
-                val snippet = snippetObj.asJsonObject["snippet"]
-                return SpotifySong("", snippet.asString, " - ", " - ", " - ", "", "")
-            }
-        } catch (e1: IOException) {
-            e1.printStackTrace()
         }
 
-        return EmptySong
+        return song ?: EmptySong
     }
 
-    private fun getSongFromCache(term: String): SpotifySong? {
-        val spotifySong: SpotifySong? = cache[term]
-        return spotifySong
+    private fun SpotifySong.setSongIsLocallyStored() {
+        this.isLocallyStored = true
+    }
+
+    private fun SpotifySong.setSongIsCacheStored() {
+        this.isCacheStored = true
     }
 }
--- src/test/java/ayds.apolo.songinfo.home.model.repository/SongRepositoryTest.kt
+++ src/test/java/ayds.apolo.songinfo.home.model.repository/SongRepositoryTest.kt
@@ -1,34 +1,84 @@
 package ayds.apolo.songinfo.home.model.repository
 
+import ayds.apolo.songinfo.home.model.entities.EmptySong
 import ayds.apolo.songinfo.home.model.entities.SpotifySong
-import ayds.apolo.songinfo.home.model.repository.external.spotify.SpotifyTrackService
+import ayds.apolo.songinfo.home.model.repository.external.spotify.SongBroker
+import ayds.apolo.songinfo.home.model.repository.local.spotify.cache.SongCache
+import ayds.apolo.songinfo.home.model.repository.local.spotify.SpotifyLocalStorage
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
 
 class SongRepositoryTest {
 
-    private val songRepository by lazy {
-        SongRepository()
+    private val songCache: SongCache = mockk(relaxUnitFun = true)
+    private val spotifyLocalStorage: SpotifyLocalStorage = mockk(relaxUnitFun = true)
+    private val songBroker: SongBroker = mockk(relaxUnitFun = true)
+
+    private val songRepository: SongRepository =
+        SongRepositoryImpl(songCache, spotifyLocalStorage, songBroker)
+
+
+    @Test
+    fun `given an existing song in cache, it should return song and mark it as stored`() {
+        val song = SpotifySong(
+            "id", "song", "artist", "album",
+            "date", "url", "image",
+            false, false
+        )
+
+        every { songCache.getSongByTerm("id") } returns song
+
+        val result = songRepository.getSongByTerm("id")
+
+        assertEquals(song, result)
+        assertTrue(song.isCacheStored)
     }
 
     @Test
-    fun `tests references`() {
+    fun `given an existing song, local storage should return song, mark it as local and update the cache`() {
+        val song = SpotifySong(
+            "id", "song", "artist", "album",
+            "date", "url", "image",
+            false, false
+        )
 
-        // Mock an object
-        val service: SpotifyTrackService = mockk()
-        val song: SpotifySong = mockk()
+        every { songCache.getSongByTerm("id") } returns null
+        every { spotifyLocalStorage.getSongByTerm("id") } returns song
 
-        // mock a response
-        every { service.getSong("title")} returns song
+        val result = songRepository.getSongByTerm("id")
 
-        // assertions
-        Assert.assertEquals(service.getSong("title"), song)
+        assertEquals(song, result)
+        assertTrue(song.isLocallyStored)
+        verify { songCache.insertSong("id", song) }
+    }
 
-        // verify mock was called
-        verify { service.getSong("title") }
+    @Test
+    fun `given an existing song, song broker should return song and update cache and storage`() {
+        val song : SpotifySong = mockk()
+
+        every { songCache.getSongByTerm("id") } returns null
+        every { spotifyLocalStorage.getSongByTerm("id") } returns null
+        every { songBroker.getSong("id") } returns song
+
+        val result = songRepository.getSongByTerm("id")
+
+        assertEquals(song, result)
+        verify { songCache.insertSong("id", song) }
+        verify { spotifyLocalStorage.insertSong("id", song) }
+    }
+
+    @Test
+    fun `given an non existing song, song broker should return empty song`() {
+        every { songCache.getSongByTerm("id") } returns null
+        every { spotifyLocalStorage.getSongByTerm("id") } returns null
+        every { songBroker.getSong("id") } returns null
+
+        val result = songRepository.getSongByTerm("id")
+
+        assertEquals(EmptySong, result)
     }
 }
